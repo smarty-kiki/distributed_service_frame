dd306213193931b97d2c733959b39eb6fb9f5931
yaoyang <kiki@smarty.so>
2018-03-08 13:44:29 +0800

    修改 demo 的功能

--- client/config/client.php
+++ client/config/client.php
@@ -2,9 +2,9 @@
 
 return [
     'distributed' => [
-        'host' => 'distributed_service',
+        'host' => 'distributed_service_frame',
         'ips' => [
-            'distributed_service',
+            'distributed_service_frame',
         ],
         'timeout' => 1,
         'retry' => 1,
--- service/demo.php
+++ service/demo.php
@@ -1,6 +1,6 @@
 <?php
 
-service('create', function ($column1 = 1, $column2 = 2)
+service('demo@create', function ($column1 = 1, $column2 = 2)
 {
     return demo::create($column1, $column2);
 });
