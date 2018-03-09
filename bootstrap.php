3842388aff0a0ddc92c7ca79e60569073d0bd319
yaoyang <kiki@smarty.so>
2018-03-09 15:15:51 +0800

    添加 cli 依赖加载功能

--- bootstrap.php
+++ bootstrap.php
@@ -6,10 +6,11 @@ date_default_timezone_set('Asia/Shanghai');
 define('ROOT_DIR', __DIR__);
 define('FRAME_DIR', ROOT_DIR.'/frame');
 define('DOMAIN_DIR', ROOT_DIR.'/domain');
+define('CLIENT_DIR', ROOT_DIR.'/client');
 define('COMMAND_DIR', ROOT_DIR.'/command');
-define('CONTROLLER_DIR', ROOT_DIR.'/controller');
 define('DEP_DOMAIN_DIR', ROOT_DIR.'/dep_domain');
 define('DEP_CLIENT_DIR', ROOT_DIR.'/dep_client');
+define('DEP_QUEUE_JOB_DIR', ROOT_DIR.'/dep_queue_job');
 
 include FRAME_DIR.'/function.php';
 include FRAME_DIR.'/otherwise.php';
@@ -19,8 +20,9 @@ config_dir(ROOT_DIR.'/config');
 unit_of_work_db_config_key('distributed');
 
 include ROOT_DIR.'/util/load.php';
-include ROOT_DIR.'/domain/load.php';
-include ROOT_DIR.'/client/client.php';
+include DOMAIN_DIR.'/load.php';
+include CLIENT_DIR.'/load.php';
 
 include DEP_DOMAIN_DIR.'/load.php';
 include DEP_CLIENT_DIR.'/load.php';
+include DEP_QUEUE_JOB_DIR.'/load.php';
