405d07a05e652963b48984457c6b456979c7d8ca
yaoyang <kiki@smarty.so>
2019-04-23 15:03:01 +0800

    使用 frame 新特性 config_midware

--- config/mysql.php
+++ config/mysql.php
@@ -1,27 +1,32 @@
 <?php
 
 return [
-    'distributed' => [
-        'charset' => 'utf8',
-        'collation' => 'utf8_unicode_ci',
-        'database' => 'default',
-        'username' => 'root',
-        'password' => 'password',
-
-        'write' => [
-            '/var/run/mysqld/mysqld.sock',
-        ],
-        'schema' => [
-            '/var/run/mysqld/mysqld.sock',
-        ],
+    'midwares' => [
+        'distributed' => 'local',
     ],
 
-    'options' => [
-        PDO::ATTR_CASE => PDO::CASE_NATURAL,
-        PDO::ATTR_ORACLE_NULLS => PDO::NULL_NATURAL,
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_STRINGIFY_FETCHES => false,
-        PDO::ATTR_EMULATE_PREPARES => false,
-        PDO::ATTR_PERSISTENT => false,
+    'resources' => [
+        'local' => [
+            'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'database' => 'default',
+            'username' => 'root',
+            'password' => 'password',
+
+            'write' => [
+                '/var/run/mysqld/mysqld.sock',
+            ],
+            'schema' => [
+                '/var/run/mysqld/mysqld.sock',
+            ],
+            'options' => [
+                PDO::ATTR_CASE => PDO::CASE_NATURAL,
+                PDO::ATTR_ORACLE_NULLS => PDO::NULL_NATURAL,
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_STRINGIFY_FETCHES => false,
+                PDO::ATTR_EMULATE_PREPARES => false,
+                PDO::ATTR_PERSISTENT => false,
+            ],
+        ],
     ],
 ];
--- config/redis.php
+++ config/redis.php
@@ -2,13 +2,18 @@
 
 return [
 
-    'idgenter' => [
-        'host' => '127.0.0.1',
-        'port' => 6379,
-        'timeout' => 1,
-        'options' => [
-            Redis::OPT_SERIALIZER => Redis::SERIALIZER_PHP,
-        ],
+    'midwares' => [
+        'idgenter' => 'local',
     ],
 
+    'resources' => [
+        'local' => [
+            'host' => '127.0.0.1',
+            'port' => 6379,
+            'timeout' => 1,
+            'options' => [
+                Redis::OPT_SERIALIZER => Redis::SERIALIZER_PHP,
+            ],
+        ],
+    ],
 ];
