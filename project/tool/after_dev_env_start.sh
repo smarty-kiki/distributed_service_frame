ef00cf81927ef21cf60f1c54b3767f2d3fd7840a
yaoyang <kiki@smarty.so>
2019-08-28 00:34:45 +0800

    after_dev_env_start 命令添加 env 参数

--- project/tool/after_dev_env_start.sh
+++ project/tool/after_dev_env_start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-php /var/www/distributed_service_frame/public/cli.php migrate:install
-php /var/www/distributed_service_frame/public/cli.php migrate
+ENV=development php /var/www/distributed_service_frame/public/cli.php migrate:install
+ENV=development php /var/www/distributed_service_frame/public/cli.php migrate
